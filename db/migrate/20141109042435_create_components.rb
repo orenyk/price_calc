1f6ae4fb0d1e04101bd5041703d48dfd299c7f11
Oren Kanner <okanner@gmail.com>
2014-11-08 23:35:12 -0500

    added associations to migrations

--- db/migrate/20141109042435_create_components.rb
+++ db/migrate/20141109042435_create_components.rb
@@ -1,6 +1,8 @@
 class CreateComponents < ActiveRecord::Migration
   def change
     create_table :components do |t|
+      t.belongs_to :product
+
       t.string :name
       t.decimal :price, :precision => 10, :scale => 2
 
